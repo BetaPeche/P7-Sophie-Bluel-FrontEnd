33cfaab9c99c36b4c2db90e00ebec2bf95b876db
BetaPeche <lolopellegrino@gmail.com>
2024-01-16 10:42:07 +0100

    Modification de la structure JS du projet

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+// Récupère et affiche les travaux via l'API 
+async function afficherTravaux (){
+    const reponse = await fetch("http://localhost:5678/api/works")
+    if (reponse.ok){
+        const avis = await reponse.json()
+        console.log(avis)
+
+        const gallery = document.querySelector('.gallery')
+
+        for (let i = 0; i < avis.length; i++) {
+            const figure = document.createElement("figure")
+            const img = document.createElement("img")
+            const figcaption = document.createElement("figcaption")
+
+            figure.setAttribute("data-category-id", avis[i].categoryId)
+            img.setAttribute("src", avis[i].imageUrl)
+            img.setAttribute("alt", avis[i].title)
+            figcaption.innerText = avis[i].title
+
+            gallery.appendChild(figure)
+            figure.appendChild(img)
+            figure.appendChild(figcaption)
+        }
+    }
+    else{
+        console.log("Pas de réponse du serveur")
+    }
+}
